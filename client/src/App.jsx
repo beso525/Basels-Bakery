03e124b530d6b5df57fe5eb96a547c827330bd2a
beso525 <139979685+beso525@users.noreply.github.com>
2026-03-15 15:54:02 -0300

    Deployment commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
   return (
     <div className='flex flex-col overflow-hidden bg-white'>
       <Routes>
-        <Route path="/" element={
+        <Route path="/auth" element={
           <CheckAuth isAuthenticated={isAuthenticated} user={user}>
             <AuthLayout />
           </CheckAuth>
